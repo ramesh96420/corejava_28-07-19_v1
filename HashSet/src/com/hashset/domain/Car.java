abc2870a2a65f4ad7c05c550ea4e791bd0b481f2
ramesh96420 <ramesh96420@gmail.com>
2019-07-28 18:38:02 +0530

    HashSet Examples

---
+++ HashSet/src/com/hashset/domain/Car.java
@@ -0,0 +1,51 @@
+package com.hashset.domain;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Car.
+ */
+public class Car {
+
+	/** The car name. */
+	private String carName;
+	
+	/** The car number. */
+	private String carNumber;
+	
+	/**
+	 * Gets the car name.
+	 *
+	 * @return the car name
+	 */
+	public String getCarName() {
+		return carName;
+	}
+	
+	/**
+	 * Sets the car name.
+	 *
+	 * @param carName the new car name
+	 */
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	
+	/**
+	 * Gets the car number.
+	 *
+	 * @return the car number
+	 */
+	public String getCarNumber() {
+		return carNumber;
+	}
+	
+	/**
+	 * Sets the car number.
+	 *
+	 * @param carNumber the new car number
+	 */
+	public void setCarNumber(String carNumber) {
+		this.carNumber = carNumber;
+	}
+	
+}
---
+++ HashSet/src/com/hashset/main/HashSetMail.java
@@ -0,0 +1,8 @@
+package com.hashset.main;
+
+public class HashSetMail {
+
+	public static void main(){
+		
+	}
+}

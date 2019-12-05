c38798798887fb41f672da3d1eecbfd49f738693
Rameshbabu <crb96java@gmail.com>
2019-12-05 10:21:52 +0530

    commit

--- SortingListAndMapObjects/src/com/sort/mapobject/RemovingDublicateElementFromMap.java
+++ SortingListAndMapObjects/src/com/sort/mapobject/RemovingDublicateElementFromMap.java
@@ -1,7 +1,10 @@
 package com.sort.mapobject;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.sort.bean.Employee;
 
 public class RemovingDublicateElementFromMap {
 
@@ -18,7 +21,30 @@ public class RemovingDublicateElementFromMap {
 		map.put("poa", 250);
 		map.put("pos", 41);
 		
-		//map.entrySet().stream().distinct()
+		System.out.println("Avoid the dublicate elements from the Map");
+		map.entrySet().stream().distinct().forEach(System.out::println);
+		
+		System.out.println("Avoid the dublicate keys from the Map");
+		map.entrySet().stream().sorted(Map.Entry.comparingByKey()).distinct().forEach(System.out::println);
+		
+		System.out.println("Avoid the dublicate values from the Map");
+		map.entrySet().stream().sorted(Map.Entry.comparingByValue()).distinct().forEach(System.out::println);
+		
+		//Employee Object
+		// Custome key Object in Map
+		Map<Employee, Integer> empMap = new HashMap<>();
+		empMap.put(new Employee(10, "Rame", "IT", 1000), 10);
+		empMap.put(new Employee(13, "OLA", "ADMIN", 5000), 50);
+		empMap.put(new Employee(11, "Jaop", "SALSE", 2000), 17);
+		empMap.put(new Employee(20, "Kopa", "HR", 11000), 12);
+		empMap.put(new Employee(12, "Lio", "IT", 3000), 100);
+		empMap.put(new Employee(22, "Kopa", "HR", 1100), 72);
+		empMap.put(new Employee(12, "OLA", "IT", 3000), 10);
+		
+		System.out.println();
+		empMap.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.comparing(Employee::getEmpSalary))).forEach(System.out::println);
+		
+		
 
 	}
 
